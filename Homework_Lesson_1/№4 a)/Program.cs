5c34128dd8adfa526e4e84b7d41476c405f4a0d6
dkudinoff <daniil.kudinoff@gmail.com>
2019-01-26 14:39:32 +0300

    + задание 5

---
+++ Homework_Lesson_1/№4 a)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// Кудинов Даниил
+/// 
+///Написать программу обмена значениями двух переменных.
+///а) с использованием третьей переменной;
+
+namespace _4_a_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите переменную a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите переменную b:");
+            int b = int.Parse(Console.ReadLine());
+            int c;
+
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine($"a = {a}, b = {b}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework_Lesson_1/№5 б)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_б_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+            Console.WriteLine("Даниил Кудинов Санкт-Петербург");
+            Console.ReadLine();
+        }
+    }
+}
